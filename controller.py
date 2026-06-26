4155a7ba12cfd22654aa106d0f1c3db59762f1bf
Takamasa Tojo <takamasa_tojo@nuco.co.jp>
2026-06-26 19:58:43 +0900

    CPU実装途中、パスの部分まで実装

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 from model import ReversiModel
+from model_cpu import ReversiModelCpu
 from view import BoardView
 
 
@@ -6,23 +7,67 @@ class ReversiController:
     def __init__(self):
         self.model=ReversiModel()
         self.view=BoardView()
+        self.model_cpu=ReversiModelCpu()
     
     def change_stone(self):
-        #put_cellはstr型なのでtuple型へ変換
         put_cell=self.view.change_stone()
         self.model.control_board(put_cell)
+        
+    def select_stone(self):
+        select_stone=self.view.select_stone()
+        if select_stone=="black":
+            decide_stone=1
+        elif select_stone=="white":
+            decide_stone=2
+        else:
+            decide_stone=None
+        return decide_stone
             
     def run(self) -> None:
-        while self.model.check_end_game==True:
+        input_partner=self.view.select_cpu_human()
+        if input_partner=="human":
+            while self.model.check_end_game==True:
+                model_board=self.model.board
+                model_turn=self.model.turn
+                model_valid_moves=self.model.valid_moves
+                model_check_input=self.model.check_input
+                self.view.print_all(model_board,model_turn,model_valid_moves,model_check_input)
+                self.change_stone()
             model_board=self.model.board
-            model_turn=self.model.turn
-            model_valid_moves=self.model.valid_moves
-            model_check_input=self.model.check_input
-            self.view.print_all(model_board,model_turn,model_valid_moves,model_check_input)
-            self.change_stone()
-        model_board=self.model.board
-        model_winner=self.model.winner
-        model_num_B=self.model.num_B
-        model_num_W=self.model.num_W
-        self.view.end_game(model_board, model_winner, model_num_B, model_num_W)
+            model_winner=self.model.winner
+            model_num_B=self.model.num_B
+            model_num_W=self.model.num_W
+            self.view.end_game(model_board, model_winner, model_num_B, model_num_W)
+        elif input_partner=="CPU":
+            decide_stone=self.select_stone()
+            if decide_stone==1:
+                while self.model_cpu.check_end_game==True:
+                        model_cpu_board=self.model_cpu.board
+                        model_cpu_turn=self.model_cpu.turn
+                        model_cpu_valid_moves=self.model_cpu.valid_moves
+                        model_cpu_check_input=self.model_cpu.check_input
+                        self.view.print_all_cpu(model_cpu_board,model_cpu_turn,model_cpu_valid_moves,model_cpu_check_input)
+                        put_cell=self.view.change_stone()
+                        self.model_cpu.control_board(put_cell)
+                        board_before_cpu=self.model_cpu.board_before_cpu
+                        valid_moves_before_cpu=self.model_cpu.valid_moves_before_cpu
+                        cpu_turn=self.model_cpu.cpu_turn
+                        self.view.print_cpu_board(board_before_cpu,valid_moves_before_cpu,cpu_turn)
+            elif decide_stone==2:
+                self.model_cpu.start_CPU()
+                while self.model_cpu.check_end_game==True:
+                        model_cpu_board=self.model_cpu.board
+                        model_cpu_turn=self.model_cpu.turn
+                        model_cpu_valid_moves=self.model_cpu.valid_moves
+                        model_cpu_check_input=self.model_cpu.check_input
+                        self.view.print_all_cpu(model_cpu_board,model_cpu_turn,model_cpu_valid_moves,model_cpu_check_input)
+                        put_cell=self.view.change_stone()
+                        self.model_cpu.control_board(put_cell)
+                        board_before_cpu=self.model_cpu.board_before_cpu
+                        valid_moves_before_cpu=self.model_cpu.valid_moves_before_cpu
+                        cpu_turn=self.model_cpu.cpu_turn
+                        self.view.print_cpu_board(board_before_cpu,valid_moves_before_cpu,cpu_turn)
+                        
+        else:
+            self.view.print_correct_answer()
         
--- view.py
+++ view.py
@@ -14,6 +14,24 @@ class BoardView:
         ["･","･","･","･","･","･","･","･"],
         ["･","･","･","･","･","･","･","･"]
         ]
+        self.board_before_cpu = [
+        ["･","･","･","･","･","･","･","･"],
+        ["･","･","･","･","･","･","･","･"],
+        ["･","･","･","･","*","･","･","･"],
+        ["･","･","･","B","W","*","･","･"],
+        ["･","･","*","W","B","･","･","･"],
+        ["･","･","･","*","･","･","･","･"],
+        ["･","･","･","･","･","･","･","･"],
+        ["･","･","･","･","･","･","･","･"]
+        ]
+    
+    def select_cpu_human(self):
+        select_partner=input("Which do you play with human or CPU?:")
+        return select_partner
+    
+    def select_stone(self):
+        human_stone=input("Which do you choose black or white?:")
+        return human_stone
     
     def print_all(self, model_board, model_turn, model_valid_moves,model_check_input):
         self.print_board(model_board)
@@ -22,10 +40,13 @@ class BoardView:
         elif model_turn == 2:
             self.turn="W"
         if model_check_input==0:
-            print("Please input correct answer")
+            self.print_correct_answer()
         print(f"current turn:{self.turn}")
         print(f"you can put your stone on {model_valid_moves}")
-    
+        
+    def print_correct_answer(self):
+        print("Please input correct answer")
+        
     def print_board(self,model_board):
         for x in range(8):
             for y in range(8):
@@ -40,6 +61,38 @@ class BoardView:
         print("  " + " ".join(str(i) for i in range(8)))
         for i, row in enumerate(self.board):
             print(f"{i} " + " ".join(row))
+        
+    def print_all_cpu(self, model_cpu_board, model_cpu_turn, model_cpu_valid_moves,model_cpu_check_input):
+        self.print_board(model_cpu_board)
+        if model_cpu_turn == 1:
+            self.turn="B"
+        elif model_cpu_turn == 2:
+            self.turn="W"
+        if model_cpu_check_input==0:
+            print("Please input correct answer")
+        print(f"current turn:{self.turn}")
+        print(f"you can put your stone on {model_cpu_valid_moves}")
+        
+    def print_cpu_board(self,board_before_cpu,valid_moves_before_cpu,cpu_turn):
+        for x in range(8):
+            for y in range(8):
+                if board_before_cpu[x][y] == 0:
+                    self.board_before_cpu[x][y] = "･"
+                elif board_before_cpu[x][y] == 1:
+                    self.board_before_cpu[x][y] = "B"
+                elif board_before_cpu[x][y] == 2:
+                    self.board_before_cpu[x][y] ="W"
+                elif board_before_cpu[x][y] == 3:
+                    self.board_before_cpu[x][y] = "*"
+        print("  " + " ".join(str(i) for i in range(8)))
+        for i, row in enumerate(self.board_before_cpu):
+            print(f"{i} " + " ".join(row))
+        if cpu_turn == 1:
+            self.cpu_turn="B"
+        elif cpu_turn == 2:
+            self.cpu_turn="W"
+        print(f"current turn:{self.cpu_turn}")
+        print(f"you can put your stone on {valid_moves_before_cpu}")
     
     def change_stone(self):
         put_cell=input("You put your stone on:")
